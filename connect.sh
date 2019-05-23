f6b1baf6e934a995040a1f94ea42a123ec6f51f6
xuyixin1 <915673544@qq.com>
2019-05-23 17:10:23 +0800

    提交执行远程脚本

---
+++ connect.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+source_base_dir="/opt/springboot/bootDemo"
+
+ssh root@47.101.42.50 << eeooff
+ ${source_base_dir}/publish.sh
+
+
+ sleep 5
+ echo "finished!"
+ sleep 2
+ #####执行完毕
+ exit
+ eeooff
